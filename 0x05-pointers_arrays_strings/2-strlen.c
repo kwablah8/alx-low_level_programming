1366c4a038f9549cbfd53eef53b6afdda0980339
93n105 <ernestkwablah19@gmail.com>
2022-12-19 19:59:05 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - function to return length of a string
+ *
+ * @s: The character containing the string
+ *
+ * Return: The legnth of the string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
