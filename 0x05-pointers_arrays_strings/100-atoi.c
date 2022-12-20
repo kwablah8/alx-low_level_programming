04e10f2d9b9dc855c923d5215b2119a11e7286eb
93n105 <ernestkwablah19@gmail.com>
2022-12-19 21:18:44 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 
 	while (s[0] != '\0')
 	{
-		if (s[0] == '_')
+		if (s[0] == '-')
 			sign_cache *= -1;
 		else if (s[0] >= '0' && s[0] <= '9')
 			result = (result * 10) + (s[0] - '0') * sign_cache;
