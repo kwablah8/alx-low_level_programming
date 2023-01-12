b73ab47c3f3da4659a80e304494484a05f2d9c84
93n105 <ernestkwablah19@gmail.com>
2023-01-11 20:33:33 -0800

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (*(s1 + s2_length) != '\0')
+	while (*(s1 + s1_length) != '\0')
 		s1_length++;
 
 	while(*(s2 + s2_length) != '\0')
@@ -36,7 +36,7 @@ char *str_concat(char *s1, char *s2)
 		return ('\0');
 
 	for (i = 0; i < s1_length; i++)
-		alloc_mem[i] = *(s1 i);
+		alloc_mem[i] = *(s1 + i);
 
 	for (j = 0; j < s2_length; j++, i++)
 		alloc_mem[i] = *(s2 + j);
